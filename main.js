91af7006306ca0254271847bc95bad09ee6a21d8
Nicolas Ferreyra <nicof2008@hotmail.com>
2023-08-07 15:08:16 -0300

    agregue las funciones

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-let edad = parseInt(prompt("Ingrese su edad:"));
-
-if(edad >= 21){
-    alert("Puede pasar");
-}else if(edad >=16){
-    let respuesta = prompt("Tenes autorizacion de tus padres");
-    if(respuesta === "Si"){
+function entrarLugar(edad){
+    if(edad >= 21){
         alert("Puede pasar");
-    }else if(respuesta === "No"){
-        alert("No puede pasar");
-    }else {alert("Texto no valido")}
-}else{alert("No puede pasar")}
+    }else if(edad >=16){
+        let respuesta = prompt("Tenes autorizacion de tus padres");
+        if(respuesta === "Si"){
+            alert("Puede pasar");
+        }else if(respuesta === "No"){
+            alert("No puede pasar");
+        }else {alert("Texto no valido")}
+    }else{alert("No puede pasar")}
+}
+function temporizador(seg){
+    for(seg == 0; seg--;){
+        alert(`${seg}`);
+    }
+}
 
+let edad = parseInt(prompt("Ingrese su edad:"));
+entrarLugar(edad);
 
 let segundos = parseInt(prompt("Cuantos segundos queres que dure la cuenta regresiva?"));
+temporizador(segundos);
 
-for(segundos == 0; segundos--;){
-    alert(`${segundos}`);
-}
